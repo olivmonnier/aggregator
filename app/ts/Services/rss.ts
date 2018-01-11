fbd91b44f59af812f2deeebb60d9d2b027a5a964
Olivier Monnier <omonnier.dev@gmail.com>
2018-01-11 14:55:42 +0100

    in progress

---
+++ app/ts/Services/rss.ts
@@ -0,0 +1,36 @@
+import { IRssItem, IFeed } from '../Interfaces/Rss';
+import { IFeedMe } from './IFeedMe';
+import * as request from 'request';
+
+const FeedMe: IFeedMe = require('feedme');
+
+function createFeedParserStream(feed: IFeed): NodeJS.WritableStream {
+  const parser = new FeedMe(true);
+
+  parser.on('title', (title: string) => {
+    feed.title = title;
+  });
+  parser.on('item', (item: IRssItem) => {
+    feed.items.push(item);
+  })
+
+  return parser;
+}
+
+export default function rss(feedUrl: string): Promise<IFeed> {
+  const feed: IFeed = {
+    title: '',
+    items: []
+  }
+
+  return new Promise<IFeed>((resolve, reject) => {
+    request.get(feedUrl)
+      .on('error', (err: Error) => {
+        reject(err)
+      })
+      .on('end', () => {
+        resolve(feed)
+      })
+      .pipe(createFeedParserStream(feed))
+  })
+}
